22251204ae27d37ee44394d2a0a12a5a14a6b029
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2018-06-20 22:36:41 +0100

    Initial commit

---
+++ PythonData/WC2018Plots/PlayerHeight.py
@@ -0,0 +1,41 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__" :
+
+    filename = r'C:\Users\owner\Git\MiscellaneousData\MiscellaneousData\output_data\WorldCup2018Squads\FIFA_tables_from_PDF.csv'
+    with open(filename, newline='', encoding='utf-8') as csvfile :
+        csvreader = csv.reader(csvfile, delimiter = ',')
+        print(csvreader)
+        fieldnames = []
+        playerInfo = []
+        for row in csvreader :
+            if len(fieldnames) == 0 :
+                fieldnames = row
+            else :
+                d = dict(zip(fieldnames, row))
+                playerInfo.append(d)
+        
+        print(len(playerInfo))
+
+pn = list(range(len(playerInfo)))
+pw = [ p['Weight'] for p in playerInfo if p['National Team'] == "England"]
+ph = [ p['Height'] for p in playerInfo if p['National Team'] == "England"]
+
+#print(ph)
+
+# Data for plotting
+#t = np.arange(0.0, 2.0, 0.01)
+#s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+#ax.plot(ph, pw)
+ax.scatter(ph, pw)
+
+#ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+#       title='About as simple as it gets, folks')
+#ax.grid()
+
+#fig.savefig("test.png")
+plt.show()
